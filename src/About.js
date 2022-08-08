61f7542b1b603a4eb0f123fbb54900fd83417d93
2-9-12-12-25 <56415603+2-9-12-12-25@users.noreply.github.com>
2022-08-07 23:32:16 -0700

    updated About  Page
    need css lmao I'll get jonathan to do it in call

--- src/About.js
+++ src/About.js
@@ -5,6 +5,18 @@ function About() {
   return (
     <div>
       <h1>About Page</h1>
+      <p>This website was designed solely to organize club information in an easily accessible manner and was designed by the 
+        Coding Outreach club in the summer of 2022. This website is home to over hundreds of clubs at CCA, and is the perfect place to find
+        the right community for you! 
+      </p>
+      <br/>
+      <h2>About Us</h2>
+      <p>Coding Outreach is a club designed to simulate real-world environments in the tech industry. At the same time, members are able to 
+        serve the community through helping develop websites, writing programs, and fulfilling technical needs for other clubs, 
+        non-profit organizations, and so on. Additionally, those who are working on projects will be able to gain programming knowledge and experience along the way.
+
+        If there are any questions or concerns about the website, please email <a href= "mailto:codingoutreach@gmail.com">codingoutreach@gmail.com</a>
+      </p>
     </div>
   );
 }
